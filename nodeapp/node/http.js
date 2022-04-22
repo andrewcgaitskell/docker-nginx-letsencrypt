66ce3a1584be85d92548d1a3b6173f01a582bec3
andrewcgaitskell <andrew@gaitskell.com>
2022-04-22 11:00:11 +0100

    Update http.js

--- nodeapp/node/http.js
+++ nodeapp/node/http.js
@@ -1,7 +1,12 @@
 var http = require('http');
+
+// Constants
+const PORT = 8080;
+const HOST = '0.0.0.0';
+
 http.createServer(function(req,res){
         res.writeHead(200, { 'Content-Type': 'text/plain' });
         res.write(req.url);
       res.end();		
-      }).listen(8080);
-console.log('Server started on localhost:8080; press Ctrl-C to terminate...!');
+      }).listen(PORT, HOST);
+console.log('Server started on 0.0.0.0:8080; press Ctrl-C to terminate...!');

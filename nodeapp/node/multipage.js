fba711a7a94eab280cc6a757b1e8ae63371f551a
andrewcgaitskell <andrew@gaitskell.com>
2022-04-22 11:04:19 +0100

    Update multipage.js

--- nodeapp/node/multipage.js
+++ nodeapp/node/multipage.js
@@ -1,6 +1,16 @@
 //include http module 
 var http = require('http');
 
+// Constants
+const PORT = 8080;
+const HOST = '0.0.0.0';
+
+http.createServer(function(req,res){
+        res.writeHead(200, { 'Content-Type': 'text/plain' });
+        res.write(req.url);
+      res.end();		
+      }).listen(PORT, HOST);
+
 http.createServer(function(req,res){
 	//store URL in variable q_string
 
@@ -30,5 +40,5 @@ http.createServer(function(req,res){
                        		res.end('Not Found');
                         	break;
 	}
-}).listen(3333);
-console.log('Server started on localhost:3333; press Ctrl-C to terminate....');
+}).listen(PORT, HOST);
+console.log('Server started on 0.0.0.0:8080; press Ctrl-C to terminate....');

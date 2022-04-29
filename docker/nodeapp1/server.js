59c9d3cf29698318133d1e3b10b3e780b9e87af4
andrewcgaitskell <andrew@gaitskell.com>
2022-04-29 11:26:46 +0100

    Create server.js

---
+++ docker/nodeapp1/server.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const express = require('express');
+
+// Constants
+const PORT = 8080;
+const HOST = '0.0.0.0';
+
+// App
+const app = express();
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
+
+//var http = require('http');
+//http.createServer(function(req,res){
+//        res.writeHead(200, { 'Content-Type': 'text/plain' });
+//        res.end('Hello World!');
+//}).listen(8080);
+
